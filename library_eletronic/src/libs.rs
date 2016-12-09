c9879491ebf5a5accd0bac0bf4dc351d7936e8df
deyvison guilherme <deyvison.guilherme@live.com>
2016-12-09 12:03:58 -0200

    Definição de módulos internos

---
+++ library_eletronic/src/libs.rs
@@ -0,0 +1,19 @@
+/*
+.-----------------------------------------------------------.
+|        Descrição das abreviações das bibliotecas          |
+|            FCC => Fórmulas de Corrente Contínua           |
+|            FCA => Fórmulas de Corrente Alternada          |
+|         FCE => Fórmulas Para Circuitos Eletrônicos        |
+|                   TRA =>Transistores                      |
+|   GCT => Grandezas Básicas de Circuitos com Transistores  |
+|           FPT =>Fórmulas Práticas Para Transistores       |
+|                     OSC => Osciladores                    |
++-----------------------------------------------------------+
+*/
+pub mod FCC;
+pub mod FCA;
+pub mod FCE;
+pub mod TRA;
+pub mod GCT;
+pub mod FPT;
+pub mod OSC;
