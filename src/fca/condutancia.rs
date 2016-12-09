0088e2e61459b18904ac28ec83b709fa604ad383
deyvison guilherme <deyvison.guilherme@live.com>
2016-12-09 17:23:00 -0200

    Add Tests

--- src/fca/condutancia.rs
+++ src/fca/condutancia.rs
@@ -1,5 +1,29 @@
-struct CE { r: f64 }
-trait Formula{ 
-    fn f_ce(&self) -> f64;
-    fn f_ma(&self) -> f64;
+#[allow(dead_code)]
+pub struct CondutanciaEletrica { 
+    pub r: f64 
+}
+#[allow(dead_code)]
+pub struct CondutanciaElemento {
+    pub r: f64,
+    pub a: f64
+}
+
+pub trait FormulaCondutanciaEletrica  {  
+    fn f_condutancia_eletrica(&self)  -> f64; 
+}
+
+pub trait FormulaCondutanciaElemento {
+    fn f_condutancia_elemento(&self)  -> f64;
+}
+
+impl FormulaCondutanciaEletrica  for CondutanciaEletrica { 
+    fn f_condutancia_eletrica(&self) -> f64 { 
+        1. / self.r 
+    } 
+}
+
+impl FormulaCondutanciaElemento for CondutanciaElemento {
+     fn f_condutancia_elemento(&self) -> f64 {
+            self.r + self.a
+     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,15 @@ pub mod tra;
 pub mod gct;
 pub mod fpt;
 pub mod osc;
+#[test]
+fn condutancia_test() {
+    use fca::condutancia;
+    use fca::condutancia::FormulaCondutanciaEletrica;
+    use fca::condutancia::FormulaCondutanciaElemento;
+
+    let x = condutancia::CondutanciaEletrica { r: 125. };
+    let y = condutancia::CondutanciaElemento { r: 125., a: 125. };
+
+    assert_eq!(0.008, x.f_condutancia_eletrica());
+    assert_eq!(250., y.f_condutancia_elemento());
+}
